3d8e625b115471862e347ef77626087565b2bc43
ckstnr99 <ckstnr99@naver.com>
2024-10-20 12:15:03 +0900

    2024.10.20. 12:14 chansook

--- src/view/MyPage/MySales/index.tsx
+++ src/view/MyPage/MySales/index.tsx
@@ -7,9 +7,18 @@ export default function MySales() {
             <div className='title'>매출 관리</div>
             <div className='line'></div>
 
-            <div className='sales-graph-box'></div>
+            <div className='sales-graph-box'>월 매출 현황</div>
 
-            <div className='sales-date-box'></div>
+            <div className='sales-date-box'>
+                <div className='selec-year-box'>
+                    <div className='selec-year'>2024 ▼</div>
+                    <div className='year-text'>년</div>
+                </div>
+                <div className='selec-month-box'>
+                    <div className='selec-month'>10 ▼</div>
+                    <div className='month-text'>월</div>
+                </div>
+            </div>
 
             <div className='completed-order-container'></div>
         </div>
