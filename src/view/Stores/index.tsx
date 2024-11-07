86d2901b2a660bdf2e95addbca1883c3388ed76a
ristukaJJang <ghwjd5585@gmail.com>
2024-11-07 09:40:07 +0900

    stores filtering

--- src/view/Stores/index.tsx
+++ src/view/Stores/index.tsx
@@ -469,7 +469,7 @@ export default function Stores() {
 
     const { storeDetails } = responseBody as GetStoreListResponseDto;
     setStoreList(storeDetails);
-    originalList.current = stores;
+    originalList.current = storeDetails;
   }
 
   //* ======================================== store main search
@@ -506,9 +506,9 @@ export default function Stores() {
       return;
     }
 
-    const { stores } = responseBody as GetStoreListResponseDto;
-    setStoreList(stores);
-    originalList.current = stores;
+    const { storeDetails } = responseBody as GetStoreListResponseDto;
+    setStoreList(storeDetails);
+    originalList.current = storeDetails;
   };
   //* ======================================== store main search
 
